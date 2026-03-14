64d7b2aba300062dc17e98d70f391b0a9231d887
Cỏ phong Long <cophonglong@gmail.com>
2026-03-15 08:49:31 +0900

    tạo icon
    icon cho phần mềm

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from tkinterdnd2 import TkinterDnD
+import tkinter as tk
 from ui.main_window import MainWindow
 
 def main():
 
     root = TkinterDnD.Tk()
+    
+    icon = tk.PhotoImage(file="assets/icon.png")
+    root.iconphoto(True, icon)
 
     root.title("Phần mềm nhập điểm")
 
